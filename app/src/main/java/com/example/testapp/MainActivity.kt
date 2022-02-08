5b7ed4d9cf420ee12298c350ac355c209f9f5434
ohsopp <wjdgus0545@naver.com>
2022-02-08 23:04:16 +0900

    alert dialog test

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -1,53 +1,91 @@
 package com.example.testapp
 
+import android.app.Dialog
+import android.content.DialogInterface
+import android.graphics.Color
 import android.graphics.Typeface
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Gravity
+import android.view.KeyEvent
+import android.view.MotionEvent
 import android.view.ViewGroup.LayoutParams.WRAP_CONTENT
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AlertDialog
 import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import com.example.testapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    val items = arrayOf<String>("사과", "배", "포도", "복숭아", "수박")
+
     lateinit var binding:ActivityMainBinding
+
+    val eventHandler = object: DialogInterface.OnClickListener{
+        override fun onClick(p0: DialogInterface?, p1: Int) {
+            if(p1 == DialogInterface.BUTTON_POSITIVE){
+                Log.d("ohspp", "OK Button")
+            }
+            else if(p1 == DialogInterface.BUTTON_NEGATIVE){
+                Log.d("ohsopp","NO Button")
+            }
+            else if(p1 == DialogInterface.BUTTON_NEUTRAL){
+                Log.d("ohsopp","MORE Button")
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
 
-        val name = TextView(this).apply{
-            typeface = Typeface.DEFAULT_BOLD
-            text = "Lake Louise"
+
+
+        binding.button.setOnClickListener {
+            createListDialog()
         }
 
-        val image = ImageView(this).also{
-            it.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.ic_launcher_foreground))
-        }
+        createDialog()
 
-        val address = TextView(this).apply{
-            typeface = Typeface.DEFAULT_BOLD
-            text = "Lake Louise, AB, Canada"
-        }
-        val layout = LinearLayout(this).apply{
-            orientation = LinearLayout.VERTICAL
-            gravity = Gravity.CENTER
-
-            addView(name, WRAP_CONTENT, WRAP_CONTENT)
-            addView(image, WRAP_CONTENT, WRAP_CONTENT)
-            addView(address, WRAP_CONTENT, WRAP_CONTENT)
-        }
-
-        binding.btn1.setOnClickListener {
-            val text1 = binding.editid.text.toString()
-            val text2 = binding.textid.text.toString()
-            Toast.makeText(applicationContext, text1+" " +text2, Toast.LENGTH_SHORT).show()
-        }
 
         setContentView(binding.root)
     }
+
+    fun showToast(str: String){
+        Toast.makeText(applicationContext, str, Toast.LENGTH_SHORT).show()
+    }
+
+    fun createDialog(){
+        AlertDialog.Builder(this).run{
+            setTitle("test dialog")
+            setIcon(android.R.drawable.ic_dialog_info)
+            setMessage("test message")
+            setPositiveButton("OK", eventHandler)
+            setNegativeButton("NO", eventHandler)
+            setNeutralButton("MORE", eventHandler)
+            show()
+        }
+    }
+
+    fun createListDialog(){
+        AlertDialog.Builder(this).run{
+            setTitle("test list dialog")
+            setIcon(android.R.drawable.ic_dialog_info)
+            setItems(items, object: DialogInterface.OnClickListener{
+                override fun onClick(p0: DialogInterface?, p1: Int) {
+                    showToast("selected fruit : ${items[p1]}")
+                }
+            })
+            setPositiveButton("OK", eventHandler)
+            show()
+        }
+    }
+
 }
