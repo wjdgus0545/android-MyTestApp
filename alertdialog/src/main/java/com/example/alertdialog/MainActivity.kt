b26324e0477b37460826a9c75852ffb4adccc5a4
ohsopp <wjdgus0545@naver.com>
2022-02-14 19:04:20 +0900

    ADD Toolbar Style

--- alertdialog/src/main/java/com/example/alertdialog/MainActivity.kt
+++ alertdialog/src/main/java/com/example/alertdialog/MainActivity.kt
@@ -76,6 +76,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
 
+        setSupportActionBar(binding.toolbar)
 
         binding.button.setOnClickListener {
             createDialog()
@@ -261,17 +262,17 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        val menuItem1: MenuItem? = menu?.add(0, 0, 0, "menu1")
-        val menuItem2: MenuItem? = menu?.add(0, 1, 0, "menu2")
+        menuInflater.inflate(R.menu.menu_main, menu)
+
         return super.onCreateOptionsMenu(menu)
     }
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.itemId){
-        0 -> {
+    override fun onOptionsItemSelected(item: MenuItem): Boolean = when (item.title){
+        "menu1" -> {
             Toast.makeText(this,"menu1 click", Toast.LENGTH_SHORT).show()
             true
         }
-        1 -> {
+        "menu2" -> {
             Toast.makeText(this, "menu2 click", Toast.LENGTH_SHORT).show()
             true
         }
