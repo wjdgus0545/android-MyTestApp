f3e7fa3143535c6f1a1ef746d7301241901bef62
ohsopp <wjdgus0545@naver.com>
2022-02-23 20:51:20 +0900

    ADD Drawer & ViewPager2 & RecyclerView with Fragment

--- alertdialog/src/main/java/com/example/alertdialog/MainActivity.kt
+++ alertdialog/src/main/java/com/example/alertdialog/MainActivity.kt
@@ -1,38 +1,77 @@
 package com.example.alertdialog
 
 import android.os.*
+import android.util.Log
+import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.widget.SearchView
 import androidx.appcompat.app.ActionBarDrawerToggle
-
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.alertdialog.databinding.ActivityMainBinding
 
+class FragmentAdapter(activity: FragmentActivity): FragmentStateAdapter(activity){
+
+    val fragments = listOf<Fragment>(OneFragment(), TwoFragment(), ThreeFragment())
+
+    override fun getItemCount(): Int {
+        return fragments.size
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        return fragments[position]
+    }
+}
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding:ActivityMainBinding
-
+    lateinit var binding: ActivityMainBinding
     lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
 
+        toggle = ActionBarDrawerToggle(this, binding.drawer, R.string.drawer_opened, R.string.drawer_closed)
+
         setContentView(binding.root)
 
-        toggle = ActionBarDrawerToggle(this, binding.drawer, R.string.drawer_opened, R.string.drawer_closed)
+        setSupportActionBar(binding.toolbar)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         toggle.syncState()
+
+        binding.viewpager.adapter = FragmentAdapter(this)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+
+        val menuItem = menu?.findItem(R.id.menu_search)
+        val searchView = menuItem?.actionView as SearchView
+
+        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
+            override fun onQueryTextChange(newText: String?): Boolean {
+                return true
+            }
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                Log.d("ohsopp", "search text : $query")
+                return true
+            }
+        })
+
+        return super.onCreateOptionsMenu(menu)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-
         if(toggle.onOptionsItemSelected(item)){
             return true
         }
         return super.onOptionsItemSelected(item)
     }
-
 }
---
+++ alertdialog/src/main/java/com/example/alertdialog/OneFragment.kt
@@ -0,0 +1,107 @@
+package com.example.alertdialog
+
+import android.content.Context
+import android.graphics.BitmapFactory
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Rect
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.view.ViewCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.alertdialog.databinding.FragmentOneBinding
+import com.example.alertdialog.databinding.ItemMainBinding
+
+
+class MyViewHolder(val binding: ItemMainBinding): RecyclerView.ViewHolder(binding.root)
+
+class MyAdapter(val datas: MutableList<String>): RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+    override fun getItemCount(): Int {
+        return datas.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return MyViewHolder(ItemMainBinding.inflate(LayoutInflater.from(parent.context),parent, false))
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val binding = (holder as MyViewHolder).binding
+
+        binding.itemText.text = datas[position]
+
+        binding.itemRoot.setOnClickListener {
+            Log.d("ohsopp", "${binding.itemText} is clicked!")
+        }
+    }
+}
+
+class MyDecoration(val context: Context): RecyclerView.ItemDecoration(){
+    override fun onDrawOver(c: Canvas, parent: RecyclerView, state: RecyclerView.State) {
+        super.onDrawOver(c, parent, state)
+
+        val width = parent.width
+        val height = parent.height
+
+        val dr: Drawable? = ResourcesCompat.getDrawable(context.resources, R.drawable.kbo, null)
+
+        val drWidth = dr?.intrinsicWidth
+        val drHeight = dr?.intrinsicHeight
+
+        val left = width/2 - drWidth?.div(2) as Int
+        val top = height/2 - drHeight?.div(2) as Int
+        c.drawBitmap(BitmapFactory.decodeResource(context.resources, R.drawable.kbo),
+        left.toFloat(), top.toFloat(), null)
+    }
+
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        super.getItemOffsets(outRect, view, parent, state)
+
+        val index = parent.getChildAdapterPosition(view) + 1
+
+        if (index % 3 == 0)
+            outRect.set(10, 10, 10, 60)
+        else
+            outRect.set(10, 10, 10, 10)
+
+        view.setBackgroundColor(Color.parseColor("#28A0FF"))
+        ViewCompat.setElevation(view, 20.0f)
+
+    }
+}
+
+class OneFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val binding = FragmentOneBinding.inflate(inflater, container, false)
+
+        val datas = mutableListOf<String>()
+
+        for(i in 1..10){
+            datas.add("item $i")
+        }
+
+        binding.recyclerView.layoutManager = LinearLayoutManager(activity)
+        val adapter = MyAdapter(datas)
+        binding.recyclerView.adapter = adapter
+        binding.recyclerView.addItemDecoration(MyDecoration(activity as Context))
+
+        return binding.root
+    }
+
+}
