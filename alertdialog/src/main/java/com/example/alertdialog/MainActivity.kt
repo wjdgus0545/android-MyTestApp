5467e7ac5e04b14f12a80159e07c7f5927dad29d
ohsopp <wjdgus0545@naver.com>
2022-02-28 17:47:31 +0900

    ADD AppBarLayout

--- alertdialog/src/main/java/com/example/alertdialog/MainActivity.kt
+++ alertdialog/src/main/java/com/example/alertdialog/MainActivity.kt
@@ -1,5 +1,8 @@
 package com.example.alertdialog
 
+
+import android.content.Intent
+import android.graphics.drawable.Drawable
 import android.os.*
 import android.util.Log
 import android.view.Menu
@@ -8,11 +11,15 @@ import android.widget.Toast
 import androidx.appcompat.widget.SearchView
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
+import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import com.example.alertdialog.databinding.ActivityMainBinding
+
 
 class FragmentAdapter(val activity: FragmentActivity): FragmentStateAdapter(activity){
 
@@ -37,15 +44,17 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
-        setSupportActionBar(binding.toolbar)
 
         toggle = ActionBarDrawerToggle(this, binding.drawer, R.string.drawer_opened, R.string.drawer_closed)
 
-        binding.viewpager.adapter = FragmentAdapter(this)
+        setSupportActionBar(binding.toolbar)
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
         toggle.syncState()
 
+
+        binding.toolbar.setNavigationIcon(R.drawable.ic_menu_hamburger)
+        binding.viewpager.adapter = FragmentAdapter(this)
 
     }
 
@@ -59,12 +68,12 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+
         val inflater = menuInflater
         inflater.inflate(R.menu.menu_main, menu)
 
         val menuItem = menu?.findItem(R.id.menu_search)
         val searchView = menuItem?.actionView as SearchView
-
         searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener{
             override fun onQueryTextChange(newText: String?): Boolean {
                 return true
@@ -78,4 +87,6 @@ class MainActivity : AppCompatActivity() {
 
         return super.onCreateOptionsMenu(menu)
     }
+
 }
+
--- alertdialog/src/main/java/com/example/alertdialog/OneFragment.kt
+++ alertdialog/src/main/java/com/example/alertdialog/OneFragment.kt
@@ -1,6 +1,8 @@
 package com.example.alertdialog
 
+import android.app.AlertDialog
 import android.content.Context
+import android.content.DialogInterface
 import android.graphics.BitmapFactory
 import android.graphics.Canvas
 import android.graphics.Color
@@ -20,12 +22,15 @@ import androidx.recyclerview.widget.RecyclerView
 import com.example.alertdialog.databinding.FragmentOneBinding
 import com.example.alertdialog.databinding.ItemMainBinding
 
+class Profiles(val img: Int, val txt: String)
 
 class MyViewHolder(val binding: ItemMainBinding): RecyclerView.ViewHolder(binding.root)
 
-class MyAdapter(val datas: MutableList<String>): RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+class MyAdapter(val datas: MutableList<Profiles>): RecyclerView.Adapter<RecyclerView.ViewHolder>(){
+    override fun getItemCount(): Int {
+        return datas.size
+    }
 
-    override fun getItemCount(): Int = datas.size
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return MyViewHolder(ItemMainBinding.inflate(LayoutInflater.from(parent.context),
         parent, false))
@@ -34,10 +39,11 @@ class MyAdapter(val datas: MutableList<String>): RecyclerView.Adapter<RecyclerVi
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         val binding = (holder as MyViewHolder).binding
 
-        binding.itemText.text = datas[position]
+        binding.itemImage.setImageResource(datas[position].img)
+        binding.itemText.text = datas[position].txt
 
-        binding.itemRoot.setOnClickListener{
-            Log.d("ohsopp", "selected : ${binding.itemText.text}")
+        binding.itemRoot.setOnClickListener {
+            Log.d("ohsopp", "selected : ${datas[position].txt}")
         }
     }
 }
@@ -72,12 +78,12 @@ class MyDecoration(val context: Context): RecyclerView.ItemDecoration(){
 
         val index = parent.getChildAdapterPosition(view) + 1
 
-        if(index % 3 == 0)
+        if (index % 3 == 0)
             outRect.set(10, 10, 10, 60)
         else
             outRect.set(10, 10, 10, 10)
 
-        view.setBackgroundColor(Color.parseColor("#28a0ff"))
+        //view.setBackgroundResource(R.drawable.three_fragment_style)
         ViewCompat.setElevation(view, 20.0f)
     }
 }
@@ -91,17 +97,45 @@ class OneFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding = FragmentOneBinding.inflate(inflater, container, false)
 
-        val datas = mutableListOf<String>()
-
-        for (i in 1..10)
-            datas.add("item $i")
+        val datas = mutableListOf<Profiles>()
 
         binding.recyclerView.layoutManager = LinearLayoutManager(inflater.context)
         val adapter = MyAdapter(datas)
         binding.recyclerView.adapter = adapter
-        binding.recyclerView.addItemDecoration(MyDecoration(activity as Context))
+        binding.recyclerView.addItemDecoration(MyDecoration(inflater.context))
 
+        binding.lionImage.setOnClickListener {
+            datas.add(0, Profiles(R.drawable.lion1, "lion"))
+            adapter.notifyDataSetChanged()
+        }
+
+        binding.personImage.setOnClickListener {
+            datas.add(0, Profiles(R.drawable.person1, "person"))
+            adapter.notifyDataSetChanged()
+        }
+
+        val eventHandler = object: DialogInterface.OnClickListener{
+            override fun onClick(p0: DialogInterface?, p1: Int) {
+                if (p1 == DialogInterface.BUTTON_POSITIVE){
+                    datas.clear()
+                    adapter.notifyDataSetChanged()
+                }
+                else if(p1 == DialogInterface.BUTTON_NEGATIVE){
+
+                }
+            }
+        }
+        binding.clear.setOnClickListener {
+
+            AlertDialog.Builder(inflater.context).run{
+                setTitle("notification")
+                setMessage("정말 삭제하시겠습니까?")
+                setIcon(android.R.drawable.ic_dialog_info)
+                setPositiveButton("네", eventHandler)
+                setNegativeButton("아니오", eventHandler)
+                show()
+            }
+        }
         return binding.root
     }
-
 }
